55a86b1ddeafdbbcfb3dc4095d06f942e1d8f5f1
david fishmand <davjfish@gmail.com>
2026-09-04 14:19:14 -0300

    init

--- app.py
+++ app.py
@@ -31,37 +31,37 @@ class MainWindow(QMainWindow):
         # initialize controls
         self.disable_controls(False)
 
-        # see if we should load some defaults for directories
-        last_source_dir = self.settings.value("last_source_dir", "")
-        last_target_dir = self.settings.value("last_target_dir", "")
-        last_sigma = self.settings.value("last_sigma", 2)
-        last_amount = self.settings.value("last_amount", 17)
-
-        self.ui.sourceDir.setText(last_source_dir)
-        self.ui.targetDir.setText(last_target_dir)
-        self.ui.sigmaSlider.setValue(int(last_sigma))
-        self.ui.amountSlider.setValue(int(last_amount))
-        self.ui.status.setText("Ready.")
-
         # initialize the options for the color map combobox
         options = [
             ("None", None),
             ("AUTUMN".title(), cv2.COLORMAP_AUTUMN),
             ("BONE".title(), cv2.COLORMAP_BONE),
-            ("COOL".title(), cv2.COLORMAP_COOL),
-            ("HOT".title(), cv2.COLORMAP_HOT),
-            ("HSV", cv2.COLORMAP_HSV),
             ("JET".title(), cv2.COLORMAP_JET),
-            ("OCEAN".title(), cv2.COLORMAP_OCEAN),
-            ("PINK".title(), cv2.COLORMAP_PINK),
-            ("RAINBOW".title(), cv2.COLORMAP_RAINBOW),
-            ("SPRING".title(), cv2.COLORMAP_SPRING),
-            ("SUMMER".title(), cv2.COLORMAP_SUMMER),
             ("WINTER".title(), cv2.COLORMAP_WINTER),
+            ("RAINBOW".title(), cv2.COLORMAP_RAINBOW),
+            ("OCEAN".title(), cv2.COLORMAP_OCEAN),
+            ("SUMMER".title(), cv2.COLORMAP_SUMMER),
+            ("SPRING".title(), cv2.COLORMAP_SPRING),
+            ("COOL".title(), cv2.COLORMAP_COOL),
+            ("HSV", cv2.COLORMAP_HSV),
+            ("PINK".title(), cv2.COLORMAP_PINK),
+            ("HOT".title(), cv2.COLORMAP_HOT),
+            ("PARULA".title(), cv2.COLORMAP_PARULA),
+            ("MAGMA".title(), cv2.COLORMAP_MAGMA),
+            ("INFERNO".title(), cv2.COLORMAP_INFERNO),
+            ("PLASMA".title(), cv2.COLORMAP_PLASMA),
+            ("VIRIDIS".title(), cv2.COLORMAP_VIRIDIS),
+            ("CIVIDIS".title(), cv2.COLORMAP_CIVIDIS),
+            ("TWILIGHT".title(), cv2.COLORMAP_TWILIGHT),
+            ("TWILIGHT_SHIFTED".title(), cv2.COLORMAP_TWILIGHT_SHIFTED),
+            ("TURBO".title(), cv2.COLORMAP_TURBO),
+            ("DEEPGREEN".title(), cv2.COLORMAP_DEEPGREEN),
         ]
         for label, value in options:
             self.ui.colorMap.addItem(label, value)
         self.ui.colorMap.currentIndexChanged.connect(self.update_image)
+
+        self.load_defaults()
 
         # set the slider values into the appropriate text boxes
         self.ui.sigmaDisplay.setText(str(self.ui.sigmaSlider.value()))
@@ -89,8 +89,10 @@ class MainWindow(QMainWindow):
         self.ui.sourceFileBrowse.clicked.connect(self.open_source_dir_picker)
         self.ui.targetFileBrowse.clicked.connect(self.open_target_dir_picker)
 
-        # connect the action exit menu action
+        # connect menu actions
         self.ui.actionExit.triggered.connect(self.close)
+        self.ui.actionAbout.triggered.connect(self.open_about_dialog)
+        self.ui.actionRestoreDefaults.triggered.connect(self.restore_defaults)
 
         # control behaviour of ok button
         self.ui.proceedButton.clicked.connect(self.validate_and_submit)
@@ -101,6 +103,41 @@ class MainWindow(QMainWindow):
 
     def cancel_processing(self):
         self.is_cancelled = True
+
+    def load_defaults(self):
+        # see if we should load some defaults for directories
+        last_source_dir = self.settings.value("last_source_dir", "")
+        last_target_dir = self.settings.value("last_target_dir", "")
+        last_sigma = self.settings.value("last_sigma", 2)
+        last_amount = self.settings.value("last_amount", 17)
+        last_color_map = self.settings.value("last_color_map", None)
+
+        self.ui.sourceDir.setText(last_source_dir)
+        self.ui.targetDir.setText(last_target_dir)
+        self.ui.sigmaSlider.setValue(int(last_sigma))
+        self.ui.amountSlider.setValue(int(last_amount))
+        color_map_idx = self.ui.colorMap.findData(last_color_map)
+        if color_map_idx > -1:
+            self.ui.colorMap.setCurrentIndex(color_map_idx)
+        self.ui.status.setText("Ready.")
+
+    def restore_defaults(self):
+        self.settings.setValue("last_source_dir", "")
+        self.settings.setValue("last_target_dir", "")
+        self.settings.setValue("last_sigma", 2)
+        self.settings.setValue("last_amount", 17)
+        self.settings.setValue("last_color_map", None)
+        self.load_defaults()
+
+    def open_about_dialog(self):
+        QMessageBox.information(self, "About this application", """
+<span>
+This application was created by Fisheries and Oceans Canada, Centre for Advice and Data Integration (CADI) of the Gulf Region.
+<br>
+<br>
+The code repository is located at:  <a href='https://github.com/dfo-gulf-science/SharperImage'>https://github.com/dfo-gulf-science/SharperImage</a>.
+</span>
+""")
 
     def validate_and_submit(self):
         # make all the fields disabled
@@ -126,8 +163,6 @@ class MainWindow(QMainWindow):
             return
 
         # let's create a memory
-        self.settings.setValue("last_source_dir", source_dir)
-        self.settings.setValue("last_target_dir", target_dir)
 
         total_files = os.listdir(source_dir).__len__()
         idx = 0
@@ -139,12 +174,20 @@ class MainWindow(QMainWindow):
         amount = self.ui.amountSlider.value()
         color_map = self.ui.colorMap.currentData()
 
+        self.settings.setValue("last_source_dir", source_dir)
+        self.settings.setValue("last_target_dir", target_dir)
+        self.settings.setValue("last_color_map", color_map)
+        self.settings.setValue("last_sigma", sigma)
+        self.settings.setValue("last_amount", amount)
+
         for f in os.listdir(source_dir):
             og_path = os.path.join(source_dir, f)
-            new_path = os.path.join(target_dir, f)
+            # we want to make sure we are not going to inadvertently overwrite the original file
+            new_f = f.split(".")[0] + "-enhanced." + f.split(".")[-1]
+            new_path = os.path.join(target_dir, new_f)
             img = cv2.imread(og_path)
             new_data = sharpen(img, sigma, amount)
-            if color_map:
+            if color_map is not None:
                 new_data = cv2.applyColorMap(new_data, color_map)
             cv2.imwrite(new_path, img=new_data)
             idx += 1
@@ -193,12 +236,10 @@ class MainWindow(QMainWindow):
     def update_sigma_display(self, value):
         self.ui.sigmaDisplay.setText(str(value))
         self.update_image()
-        self.settings.setValue("last_sigma", str(value))
 
     def update_amount_display(self, value):
         self.ui.amountDisplay.setText(str(value))
         self.update_image()
-        self.settings.setValue("last_amount", str(value))
 
     def validate_sigma_text(self):
         current_text = self.ui.sigmaDisplay.text()
@@ -224,7 +265,7 @@ class MainWindow(QMainWindow):
         original_data = cv2.imread(self.original_filename)
         new_data = sharpen(original_data, sigma, amount)
         color_map = self.ui.colorMap.currentData()
-        if color_map:
+        if color_map is not None:
             new_data = cv2.applyColorMap(new_data, color_map)
 
         # 1. Compress the OpenCV BGR array into a JPG/PNG memory buffer
@@ -239,7 +280,6 @@ class MainWindow(QMainWindow):
             Qt.TransformationMode.SmoothTransformation
         )
         self.ui.newImage.setImage(new_pixmap)
-
 
 
 if __name__ == "__main__":
